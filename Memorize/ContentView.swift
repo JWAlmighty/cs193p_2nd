6badcf109dbc8d6f49c947d09adf398afe36bee6
JWAlmighty <jwele2218@snu.ac.kr>
2024-03-19 23:41:46 +0900

    lec 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,17 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: Array<String> = ["💋", "🥰", "😍", "😘", "👩‍❤️‍👨", "♥️"]
+    @State var cardCount: Int = 1
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            ScrollView{
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+            
         }
         .padding()
+        
     }
+    
+    var cards: some View{
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id:\.self){
+                index in CardView(isFaceUp: true, content: emojis[index])
+                    .aspectRatio(2/3, contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    var cardCountAdjusters: some View{
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View{
+        Button(action: {
+            cardCount += offset
+        },
+               label: {
+            Image(systemName: symbol)
+        })
+            .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View{
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdder: some View{
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+    
 }
 
+struct CardView: View{
+    @State var isFaceUp: Bool = false
+    let content: String
+    var body: some View{
+        ZStack{
+            let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture{
+            isFaceUp.toggle()
+        }
+    }
+}
 #Preview {
     ContentView()
 }
